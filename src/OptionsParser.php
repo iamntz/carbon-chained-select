4f11811c782081667188c2e8a9dbf194a4afd541
Ionuț Staicu <github@iamntz.com>
2018-07-02 16:38:07 +0300

    changed the config format to allow more flexible config

--- src/OptionsParser.php
+++ src/OptionsParser.php
@@ -14,58 +14,94 @@ class OptionsParser
 		$this->options = array_merge([
 			'selectOptions' => [],
 			'stringifyValue' => false,
-			'defaultLabel' => null,
 		], $options);
 
 		$this->config = apply_filters('carbon_chained_select_config', $this->config);
 
-    if (!is_null($fieldName)) {
+		if (!is_null($fieldName)) {
 			$this->config = apply_filters("carbon_chained_select_config/name={$fieldName}", $this->config);
 		}
 	}
 
+	/**
+	 * The idea is to have an array following this structure:
+	 *
+	 * [
+	 * 		'config' => [
+	 * 			'label' => '',
+	 * 			'option_label' => '',
+	 * 			'name' => ''
+	 * 			'multiple' => true/false
+	 * 		],
+	 *
+	 * 		'options' => [
+	 * 			[
+	 * 				'value' => ''
+	 * 				'label' => '',
+	 * 				'child' => [] <<< an array similar to the parent array
+	 * 			]
+	 * 		]
+	 * ]
+	 */
+
 	public function parse()
 	{
-		$parsed = [];
-
 		if (is_callable($this->options['selectOptions'])) {
 			$this->options['selectOptions'] = call_user_func($this->options['selectOptions']);
 		}
 
-		foreach ($this->options['selectOptions'] as $key => $value) {
-			if ($key === $this->config['labelKey']) {
-				$parsed['label'] = $value;
-				continue;
-			}
+		$parsed = [];
 
-			if ($key === ($this->config['configKey'] ?? false)) {
-				$parsed['config'] = $value;
+		$parsed['config'] = [
+			'multiple' => false,
+			'endpoint' => '',
+		];
+
+		$config = $this->getConfig($this->options['selectOptions']);
+
+		$parsed['config'] = array_merge(($parsed['config']), $config);
+		$parsed['config']['label'] = $parsed['config']['label'] ?? $this->getLabel($this->options['selectOptions']);
+		$parsed['config']['option_label'] = $parsed['config']['option_label'] ?? $parsed['config']['label'];
+
+		foreach ($this->options['selectOptions'] as $key => $value) {
+			if (in_array($key, [$this->config['labelKey'], $this->config['configKey']])) {
 				continue;
 			}
 
 			$option = [
+				'child' => null,
 				'value' => $this->options['stringifyValue'] ? strval($key) : $key,
-				'label' => $this->options['defaultLabel'] ?? $key ?? '',
-				'child' => [],
 			];
 
 			if (is_array($value)) {
-				$option['label'] = $value[$this->config['labelKey']] ?? $this->options['defaultLabel'] ?? $key;
+				$optionConfig = $this->getConfig($value);
+				$option['label'] = $optionConfig['option_label'] ?? $optionConfig['label'] ?? $value[$this->config['labelKey']] ?? $key;
 
 				$child = new self([
 					'selectOptions' => $value[0] ?? $value,
 					'stringifyValue' => $this->options['stringifyValue'],
-					'defaultLabel' => $option['label'],
 				]);
 
 				$option['child'] = $child->parse();
 			} else {
-				$option['label'] = strval($value) ?? $key ?? $this->options['defaultLabel'];
+				$option['label'] = strval($value) ?? $key;
+				$parsed['config']['name'] = $parsed['config']['name'] ?? $option['value'];
 			}
+
 
 			$parsed['options'][] = $option;
 		}
 
 		return $parsed;
+	}
+
+	protected function getConfig($el)
+	{
+		return $el[$this->config['configKey']] ?? [];
+	}
+
+	protected function getLabel($el, $default = null)
+	{
+		return $el[$this->config['labelKey']] ?? $default;
 	}
 }
